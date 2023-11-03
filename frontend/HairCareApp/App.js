783b9dbd55c5533459e6f6657559375b0f34199e
aghinea <aghinea@bitdefender.com>
2023-11-03 16:46:20 +0200

    Fixed App.js

--- frontend/HairCareApp/App.js
+++ frontend/HairCareApp/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import './App.css';
+import { Text, View, StyleSheet } from 'react-native';
 
 function App() {
-  return (
-      <View className="App">
-        <Text>
-          Welcome to HairCareApp!
-        </Text>
-      </View>
-  );
+    return (
+        <View style={styles.app}>
+            <Text style={styles.text}>
+                Welcome to HairCareApp!
+            </Text>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    app: {
+        // Your app styles go here, for example:
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    text: {
+        // Your text styles go here, for example:
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+});
 
 export default App;
