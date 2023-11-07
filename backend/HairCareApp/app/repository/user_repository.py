4f96450b9cc5e6a8a86381164800071f3f998575
Timea <timeanilgesz01@gmail.com>
2023-11-07 22:32:44 +0200

    Created DAl for backend

---
+++ backend/HairCareApp/app/repository/user_repository.py
@@ -0,0 +1,18 @@
+from app.dal.user_dal import UserDAL
+from app.dto.user_dto import UserCreateDTO, UserResponseDTO
+
+
+class UserRepository:
+    def __init__(self):
+        self.dal = UserDAL()
+
+    def register_user(self, user_dto: UserCreateDTO) -> UserResponseDTO:
+        # UserCreateDTO should already contain the password hashed
+        user_data = user_dto.model_dump()
+        user_data["user_id"] = self._generate_user_id()
+        self.dal.add_user(user_data)
+        return UserResponseDTO(**user_data)
+
+    def _generate_user_id(self) -> str:
+        # TODO: Implement user ID generation logic
+        return "some_unique_user_id"
