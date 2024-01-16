3e81e7f2229dbf4f7a4cabcc3fdc24f78f24bb89
Timea <timeanilgesz01@gmail.com>
2024-01-16 18:54:32 +0200

    Updated UI

--- frontend/HairCareApp/src/pages/HairProductsScreen.js
+++ frontend/HairCareApp/src/pages/HairProductsScreen.js
@@ -9,20 +9,20 @@ const HairProductsScreen = ({navigation}) => {
     const ItemSeparator = () => <View style={{height: 20}}/>; // Adjust height for desired spacing
 
     const products = [
-        {id: '1', name: 'Shampoo 1', type: 'Shampoo', price: '$20', description: 'Description of Shampoo 1',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
-        {id: '2', name: 'Shampoo 2', type: 'Shampoo', price: '$22', description: 'Description of Shampoo 2',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
-        {id: '3', name: 'Shampoo 3', type: 'Shampoo', price: '$24', description: 'Description of Shampoo 3',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
-        {id: '4', name: 'Shampoo 4', type: 'Shampoo', price: '$26', description: 'Description of Shampoo 4',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
-        {id: '7', name: 'Conditioner 2', type: 'Conditioner', price: '$32', description: 'Description of Conditioner 2',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
-        {id: '8', name: 'Conditioner 3', type: 'Conditioner', price: '$34', description: 'Description of Conditioner 3',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
-        {id: '9', name: 'Conditioner 4', type: 'Conditioner', price: '$36', description: 'Description of Conditioner 4',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
-        {id: '10', name: 'Conditioner 5', type: 'Conditioner', price: '$38', description: 'Description of Conditioner 5',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
-        {id: '5', name: 'Shampoo 5', type: 'Shampoo', price: '$28', description: 'Description of Shampoo 5',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
-        {id: '11', name: 'Styling 1', type: 'Styling', price: '$40', description: 'Description of Styling 1',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
-        {id: '12', name: 'Styling 2', type: 'Styling', price: '$42', description: 'Description of Styling 2',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
-        {id: '13', name: 'Styling 3', type: 'Styling', price: '$44', description: 'Description of Styling 3',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
-        {id: '14', name: 'Styling 4', type: 'Styling', price: '$46', description: 'Description of Styling 4',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
-        {id: '15', name: 'Styling 5', type: 'Styling', price: '$48', description: 'Description of Styling 5',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
+        {id: '1', productName: 'Shampoo 1', type: 'Shampoo', price: '$20', description: 'Description of Shampoo 1',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
+        {id: '2', productName: 'Shampoo 2', type: 'Shampoo', price: '$22', description: 'Description of Shampoo 2',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
+        {id: '3', productName: 'Shampoo 3', type: 'Shampoo', price: '$24', description: 'Description of Shampoo 3',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
+        {id: '4', productName: 'Shampoo 4', type: 'Shampoo', price: '$26', description: 'Description of Shampoo 4',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
+        {id: '7', productName: 'Conditioner 2', type: 'Conditioner', price: '$32', description: 'Description of Conditioner 2',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
+        {id: '8', productName: 'Conditioner 3', type: 'Conditioner', price: '$34', description: 'Description of Conditioner 3',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
+        {id: '9', productName: 'Conditioner 4', type: 'Conditioner', price: '$36', description: 'Description of Conditioner 4',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
+        {id: '10', productName: 'Conditioner 5', type: 'Conditioner', price: '$38', description: 'Description of Conditioner 5',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
+        {id: '5', productName: 'Shampoo 5', type: 'Shampoo', price: '$28', description: 'Description of Shampoo 5',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
+        {id: '11', productName: 'Styling 1', type: 'Styling', price: '$40', description: 'Description of Styling 1',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
+        {id: '12', productName: 'Styling 2', type: 'Styling', price: '$42', description: 'Description of Styling 2',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
+        {id: '13', productName: 'Styling 3', type: 'Styling', price: '$44', description: 'Description of Styling 3',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
+        {id: '14', productName: 'Styling 4', type: 'Styling', price: '$46', description: 'Description of Styling 4',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
+        {id: '15', productName: 'Styling 5', type: 'Styling', price: '$48', description: 'Description of Styling 5',url:'https://olaplex.com/products/no-0-intensive-bond-building-treatment-2021'},
 
     ];
 
